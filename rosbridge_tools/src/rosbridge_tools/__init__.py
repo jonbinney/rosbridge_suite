7d31abac4d66c25b7a14fda71e25a493acc93579
Jon Binney <jon.binney@gmail.com>
2014-12-17 19:15:09 -0800

    Function in robridge_tools for importing tornado

--- rosbridge_tools/src/rosbridge_tools/__init__.py
+++ rosbridge_tools/src/rosbridge_tools/__init__.py
@@ -1,4 +1,22 @@
 import backports
 import backports.ssl_match_hostname
-import tornado
-import tornado.platform
+
+def find_tornado():
+    import sys
+    import os.path
+    import rospkg
+    rpkg = rospkg.RosPack()
+    sys.path = [
+        os.path.split(__file__)[0],
+        os.path.join(rpkg.get_path('rosbridge_tools'), 'src/rosbridge_tools'),
+        ] + sys.path
+    print sys.path
+    import tornado
+    import tornado.platform
+    import tornado.ioloop
+    import tornado.web
+    import tornado.websocket
+    sys.path = sys.path[2:]
+    assert(tornado.version == '4.0.2')
+    return tornado
+
